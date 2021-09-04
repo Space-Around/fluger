db257447472862c155523f007038b23b45ae3157
sosdan73 <danyava13@gmail.com>
2021-09-04 15:15:40 +0300

    adds a chart to the company page

---
+++ frontend/src/components/Chart.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <apexchart type="line" :options="options" :series="series"></apexchart>
+  </div>
+</template>
+
+<script>
+  import vueHighcharts from 'highcharts-vue'
+
+  export default {
+    props: ['x-axis', 'y-axis'],
+    data() {
+      return {
+        
+      }
+    },
+    computed: {
+      options() {
+        return {
+          chart: {
+            id: 'company information'
+          },
+          xaxis: {
+            categories: this.xAxis
+          }
+        }
+      },
+      series() {
+        return [{
+          name: 'Цена',
+          data: this.yAxis
+        }]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ frontend/src/views/Company.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="company">
+    <chart/>
+  </div>
+</template>
+
+<script>
+import chart from "../components/Chart.vue"
+
+export default {
+  data() {
+    return {
+      activeCompany: {
+        name: '',
+        metaInfo: {},
+        predictionsData: {
+          2021: 617.0901875,
+          2022: 656.3474277,
+          2023: 695.60466789,
+          2024: 734.86190809,
+          2025: 774.11914828,
+        }
+      }
+    }
+  },
+  components: {
+    chart,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
