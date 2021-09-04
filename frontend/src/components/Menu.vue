bdae5b4579553629701c49d1ba1bd7e423392b7f
transhumanity-adept <d.b_official@mail.ru>
2021-09-04 16:43:16 +0300

    added tooltips

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -1,79 +1,113 @@
 <template>
-    <div class="d-flex menu">
-        <div class="d-flex pa-4 menu-left">
-            <v-icon large class="menu-left-icon">
-                mdi-account-multiple
-            </v-icon>
-            <v-icon large class="menu-left-icon">
-                mdi-account-question
-            </v-icon>
-            <v-icon @click="clickIcon" large class="menu-left-icon">
-                mdi-currency-usd 
-            </v-icon>
-        </div>
-        <div v-if="showMenu" class="d-flex menu-right pa-4">
-            <h3>Header</h3>
-            <ul class="d-flex list">
-                <li><router-link to="/company" class="link">One</router-link></li>
-                <li><router-link to="/company" class="link">Two</router-link></li>
-                <li><router-link to="/company" class="link">Three</router-link></li>
-            </ul>
-        </div>
+  <div class="d-flex menu">
+    <div class="d-flex pa-4 menu-left">
+      <v-tooltip right>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon
+            color="blue"
+            large
+            v-bind="attrs"
+            v-on="on"
+            class="menu-left-icon"
+          >
+            mdi-account-multiple
+          </v-icon>
+        </template>
+        <span>Профиль</span>
+      </v-tooltip>
+      <v-tooltip right>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon
+            color="blue"
+            v-bind="attrs"
+            v-on="on"
+            large
+            class="menu-left-icon"
+          >
+            mdi-account-question
+          </v-icon>
+        </template>
+        <span>Тестирование</span>
+      </v-tooltip>
+      <v-tooltip right>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon
+            color="green"
+            large
+            v-bind="attrs"
+            v-on="on"
+            class="menu-left-icon"
+            @click="clickIcon"
+          >
+            mdi-currency-usd
+          </v-icon>
+        </template>
+        <span>Компании</span>
+      </v-tooltip>
     </div>
+    <div v-if="showMenu" class="d-flex menu-right pa-4">
+      <h3>Header</h3>
+      <ul class="d-flex list">
+        <li><router-link to="/company" class="link">One</router-link></li>
+        <li><router-link to="/company" class="link">Two</router-link></li>
+        <li><router-link to="/company" class="link">Three</router-link></li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                showMenu: false,
-            }
-        },
-        methods: {
-            clickIcon() {
-                this.showMenu = !this.showMenu;
-            }
-        }
-    }
+export default {
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+  methods: {
+    clickIcon() {
+      this.showMenu = !this.showMenu;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    .menu {
-        position: absolute;
-        flex-direction: columns;
-        height: 100%;
-    }
-    .list {
-        padding-left: 0;
-    }
-    .menu-left {
-        flex-direction: column;
-        width: 70px;
-    }
-    .menu-left-icon {
-        margin-bottom: 15px;
-    }
-    .menu-right {
-        background-color: #e8e8e8;
-        flex-direction: column;
-        width: 230px;
-        overflow-y: auto;
-    }
-    .menu-right h3 {
-        text-align: center;
-    }
-    .menu-right ul {
-        flex-direction: column;
-        list-style: none;
-        width: 100%;
-    }
-    .link {
-        display: flex;
-        align-items: center;
-        color: black;
-        text-decoration: none;
-        width: 100%;
-        height: 35px;
-        margin-bottom: 10px;
-    }
+.menu {
+  position: absolute;
+  flex-direction: columns;
+  height: 100%;
+}
+.list {
+  padding-left: 0;
+}
+.menu-left {
+  flex-direction: column;
+  width: 70px;
+}
+.menu-left-icon {
+  margin-bottom: 15px;
+}
+.menu-right {
+  background-color: #e8e8e8;
+  flex-direction: column;
+  width: 230px;
+  overflow-y: auto;
+}
+.menu-right h3 {
+  text-align: center;
+}
+.menu-right ul {
+  flex-direction: column;
+  list-style: none;
+  width: 100%;
+}
+.link {
+  display: flex;
+  align-items: center;
+  color: black;
+  text-decoration: none;
+  width: 100%;
+  height: 35px;
+  margin-bottom: 10px;
+}
 </style>
